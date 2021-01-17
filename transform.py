81cf478add3daa545a008b34c5c63c2cb1d90702
GPandita <gauravpandita98@gmail.com>
2021-01-17 11:46:17 +0530

    Change transform.py Add test.py

--- transform.py
+++ transform.py
@@ -1,53 +1,48 @@
-# Author: Gaurav Pandita
-# Created On : 16 January 2021
-# Python 3
-import json
 import csv
+import json
+import sys
 
-# fetch input file path and output directory from paths.json
-with open("paths.json", "r") as config_file:
-    data = json.load(config_file)
-    input_file_path = data['input_file_path']
-    outfile_directory = data['output_directory']
-    config_file.close()
+def read_json_file(input_file_path):
+    with open(input_file_path, "r") as read_file:
+        data = json.load(read_file)
+    print("\n**** Successfully read JSON file ****\n")
+    return data
 
-# opening tracker_wise.json
-with open(input_file_path, "r") as read_file:
-    data = json.load(read_file)
+def transform_to_frame_wise(data):
+    temp = data['answer_key']['video2d']['data']
+    temp['frames'] = {}
 
-# creating a new dictionary for creating frame_wise json
-new_data = {"answer_key":{"video2d":{"data":{"frames":{}}}}}
-
-# opeing output.csv for filling in ["frame_id", "tracking_id", "label"] 
-with open(outfile_directory + "output.csv", "w") as outfile_csv:
-    # creating a csv_writer for writing to csv file
-    writer = csv.writer(outfile_csv)
-    writer.writerow(["frame_id", "tracking_id", "label"])
-
-    # iterating over every tracker
-    for tracker in data['answer_key']['video2d']['data']['trackers']:
-        # iterating over every frame
+    for tracker in temp['trackers']:
         for frame in tracker['frames']:
-            if frame not in new_data['answer_key']['video2d']['data']['frames'].keys():
-                new_data['answer_key']['video2d']['data']['frames'][frame] = []
-            # creating a dictionary with tracker_id as key (first key in the frame_wise json)
+            if frame not in temp['frames'].keys():
+                temp['frames'][frame] = []
             d = {'tracker_id':tracker['_id']}
-            # appending the tracker_id dictionary to the frames list
-            new_data['answer_key']['video2d']['data']['frames'][frame].append(d)
-            # adding all other elements to lastly added dictionary to the frame list as it is (as they don`t need to be modified)
-            new_data['answer_key']['video2d']['data']['frames'][frame][-1].update(**tracker['frames'][frame])
-            new_data['answer_key']['video2d']['data']['frames'][frame][-1]['color'] = tracker['color']
-            # write out tracker_id, frame_id and label to csv file
-            writer.writerow([frame, tracker['_id'], tracker['frames'][frame]["label"]])
+            temp['frames'][frame].append(d)
+            temp['frames'][frame][-1].update(**tracker['frames'][frame])
+            temp['frames'][frame][-1]['color'] = tracker['color']
 
-    read_file.close()
+    del temp['trackers']
+    return data
 
-# opening output.json file in output directory to write out frame_wise json
-with open(outfile_directory + "output.json", "w") as outfile_json:
-    # converting dict to json format
-    json.dump(new_data, outfile_json)
+def write_json_file(data):
+    with open("output/output1.json", "w") as outfile_json:
+        json.dump(data, outfile_json)
     outfile_json.close()
+    print("\n**** Transformed JSON file. Written to output directory as output.json ****\n")
 
-# clearing out used dictionaries
-data.clear()
-new_data.clear()
+def generate_csv_file(data):
+    with open("output/output1.csv", "w") as outfile_csv:
+#        writer = csv.writer(outfile_csv)
+#        writer.writerow(["frame_id", "tracking_id", "label"])
+        temp = data['answer_key']['video2d']['data']
+        for frames in temp['frames']:
+            for frame in frames:
+                continue
+#                print(frame)
+#        writer.writerow([frame, tracker['_id'], tracker['frames'][frame]["label"]])
+
+if __name__ == "__main__":
+    data = read_json_file(sys.argv[1])
+    data = transform_to_frame_wise(data)
+    write_json_file(data)
+    generate_csv_file(data)
