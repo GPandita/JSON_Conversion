2a771bc5c4fbe7ce6cc4f0e5d86e82edf040d668
GPandita <gauravpandita98@gmail.com>
2021-01-18 19:04:20 +0530

    Add logs

--- main.py
+++ main.py
@@ -1,81 +1,95 @@
 """
 Created on Sat Jan 16
-
-@author: Gaurav Pandita
+@author: Gaurav Panditad
 """
 
 import csv
 import json
+import logging
+import os
 import sys
 
+
 def read_json_file(input_file_path):
-    try:
-        with open(input_file_path, "r") as read_file:
-            data = json.load(read_file)
-        print("\n**** Read JSON file " + input_file_path + " ****\n")
-        return data
-    except:
-        print("\n---- Error while reading the json file. Please check the input file ----\n")
-        sys.exit()
+    with open(input_file_path, "r") as read_file:
+        data = json.load(read_file)
+    logging.info("Read Input JSON (" + input_file_path + ") - SUCCESS")
+    return data
+
 
 def transform_to_frame_wise(data):
-    try:
-        # data_ptr is like a pointer referencing data dictionary
-        data_ptr = data['answer_key']['video2d']['data']
-        # creating frames entry in data dictionary for adding frame_wise data
-        data_ptr['frames'] = {}
+    # data_ptr is like a pointer referencing data dictionary
+    data_ptr = data['answer_key']['video2d']['data']
+    # creating frames entry in data dictionary for adding frame_wise data
+    data_ptr['frames'] = {}
 
-        # iterating over every tracker entry
-        for tracker in data_ptr['trackers']:
-            # iterating over every frame in a tracker
-            for frame in tracker['frames']:
-                # if first entry for a frame
-                if frame not in data_ptr['frames'].keys():
-                    data_ptr['frames'][frame] = []
+    # iterating over every tracker entry
+    for tracker in data_ptr['trackers']:
+        # iterating over every frame in a tracker
+        for frame in tracker['frames']:
+            # if first entry for a frame
+            if frame not in data_ptr['frames'].keys():
+                data_ptr['frames'][frame] = []
                 
-                data_ptr['frames'][frame].append({'tracker_id':tracker['_id']})
-                data_ptr['frames'][frame][-1].update(**tracker['frames'][frame])
-                data_ptr['frames'][frame][-1]['color'] = tracker['color']
+            data_ptr['frames'][frame].append({'tracker_id':tracker['_id']})
+            data_ptr['frames'][frame][-1].update(**tracker['frames'][frame])
+            data_ptr['frames'][frame][-1]['color'] = tracker['color']
 
-        # remove tracker_wise data
-        del data_ptr['trackers']
-        return data
-    except:
-        print("\n---- Error while transforming the json. Please verify the input json file ----\n")
-        sys.exit()
+    # remove tracker_wise data
+    del data_ptr['trackers']
+    logging.info("Transform JSON - SUCCESS")
+    return data
+
 
 def write_json_file(data):
-    try:
-        with open("output/output.json", "w") as outfile_json:
-            json.dump(data, outfile_json)
-        outfile_json.close()
-        print("\n**** Transformed JSON file. Written to output directory as output.json ****\n")
-    except:
-        print("\n---- Error while writing output json file. Please check if output directory exists ----\n")
-        sys.exit()
+    # if output directory doesn't exists then create one
+    if not os.path.exists('output'):
+        os.makedirs('output')
+    with open("output/output.json", "w") as outfile_json:
+        json.dump(data, outfile_json)
+    outfile_json.close()
+    logging.info("JSON File Write - SUCCESS (output/output.json)")
+
 
 def generate_csv_file(data):
-    try:
-        with open("output/output.csv", "w") as outfile_csv:
-            writer = csv.writer(outfile_csv)
-            writer.writerow(["frame_id", "tracking_id", "label"])
-            for tracker in data['answer_key']['video2d']['data']['trackers']:
-                for frame in tracker['frames']:
-                    writer.writerow([frame, tracker['_id'], tracker['frames'][frame]["label"]])
-        outfile_csv.close()
-        print("\n**** CSV file generated. Written to output directory as output.csv ****\n")
-    except:
-        print("\n---- Error while writing output csv file. Please check if output directory exists ----\n")
-        sys.exit()
+    if not os.path.exists('output'):
+        os.makedirs('output')
+    with open("output/output.csv", "w") as outfile_csv:
+        writer = csv.writer(outfile_csv)
+        writer.writerow(["frame_id", "tracking_id", "label"])
+        for tracker in data['answer_key']['video2d']['data']['trackers']:
+            for frame in tracker['frames']:
+                writer.writerow([frame, tracker['_id'], tracker['frames'][frame]["label"]])
+    outfile_csv.close()
+    logging.info("CSV Generate & Wirte - SUCCESS (output/output.csv)")
 
 
 if __name__ == "__main__":
+    # if logs directory doesn't exist then create one
+    if not os.path.exists('logs'):
+        os.makedirs('logs')
+    # setting logging configuration
+    logging.basicConfig(filename="logs/main.log", level=logging.INFO, format='%(asctime)s - %(process)d - %(levelname)s  \t- %(message)s')
+
     if len(sys.argv) != 2:
-        print("\nRun the file as : [python] main.py [input_json_file_path]\n")
-    else:
+        print("Run as : [python] main.py [input_json_file_path]")
+        sys.exit()
+    try:
+        logging.info("**** STARTING ****")
         data = read_json_file(sys.argv[1])
         generate_csv_file(data)
         data = transform_to_frame_wise(data)
         write_json_file(data)
         data.clear()
-        print("\n**** Successful ****\n")
+        print("**** SUCCESSFUL ****")
+        logging.info("**** SUCCESSFUL ****")
+    except Exception as error:
+        exception_message = str(error)
+        exception_type, exception_object, exception_traceback = sys.exc_info()
+        logging.error(f"{exception_message} - {exception_type} - Line {exception_traceback.tb_lineno}")
+        logging.info("**** UNSUCCESSFUL ****")
+        print("**** UNSUCCESSFUL (For more information check logs/main.log) ****")
+else:
+    if not os.path.exists('logs'):
+        os.makedirs('logs')
+    logging.basicConfig(filename="logs/test.log", level=logging.INFO, format='%(asctime)s - %(process)d - %(levelname)s  \t- %(message)s')
--- test.py
+++ test.py
@@ -1,20 +1,33 @@
 """
 Created on Sat Jan 17
-
 @author: Gaurav Pandita
 """
+
+import logging
+import sys
 import unittest
-from main import *
+from main import read_json_file, transform_to_frame_wise
+
 
 class TestJsonConversion(unittest.TestCase):
     def test_transform(self):
-        # input file in test drectory
-        input_data = read_json_file("test/input_test.json")
-        # transformed output
-        output_data = transform_to_frame_wise(input_data)
-        # known correct test case output
-        original_output_data = read_json_file("test/output_test.json")
-        self.assertDictEqual(output_data, original_output_data)
+        try:
+            # input file in test drectory
+            input_data = read_json_file("test/input_test.json")
+            # transformed output
+            output_data = transform_to_frame_wise(input_data)
+            # known correct test case output
+            original_output_data = read_json_file("test/output_test.json")
+            self.assertDictEqual(output_data, original_output_data)
+        except Exception as error:
+            exception_message = str(error)
+            exception_type, exception_object, exception_traceback = sys.exc_info()
+            logging.error(f"{exception_message} - {exception_type} - Line {exception_traceback.tb_lineno}")
+            logging.error("**** TEST CASE - test_transform - Fail ****")
+            raise error
+        logging.info("**** TEST CASE - test_transform - Pass ****")
+
 
 if __name__ == "__main__":
+    logging.info("**** TEST START ****")
     unittest.main()
